d2392bb61a5d4dd8efe4f134b0cb597f6854608c
Asmat1k <dobrovti@mail.ru>
2023-12-01 16:06:37 +0300

    feat: add output info on main screen with img

--- react-form/src/pages/main/index.tsx
+++ react-form/src/pages/main/index.tsx
@@ -5,16 +5,31 @@ import { useAppSelector } from '../../app/appHooks';
 
 function Main() {
   const { data } = useAppSelector((state) => state.userReducer);
-  console.log(data);
   return (
     <section className={styles.container}>
-      <div className={styles.wrapper}>
+      <div className={styles.list}>
         <Link className={styles.link} to="/form1">
           <span className={styles.text}>Go to form1</span>
         </Link>
         <Link className={styles.link} to="/form2">
           <span className={styles.text}>Go to form2</span>
         </Link>
+      </div>
+      <div className={styles.info}>
+        {data['name'] &&
+          Object.entries(data).map((item, index) => {
+            const [key, value] = item;
+            if (key === 'file' || key === 'checkbox' || key === 'cPassword')
+              return;
+            if (key === 'fileBase64')
+              return <img key={index} src={value as string} alt="" />;
+            return (
+              <div key={index} className={styles.data}>
+                <div className={styles.title}>{key}: </div>
+                <div className={styles.value}>{value}</div>
+              </div>
+            );
+          })}
       </div>
     </section>
   );
